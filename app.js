e94c16cbdaa3abc641597fb66df1d7b356085f36
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2022-08-30 13:42:08 -0400

    generate an html file w/ link to github page

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-
-//Arrow function with loop
-const printProfileData  = profileDataArr => {
-
-    // This...
-    for (let i = 0; i < profileDataArr.length; i ++) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('================');
-
-    // Is the same as this... 
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem);
-    });
-
-    //And, that last one could also be written this way: 
-    // profileDataArr.forEach(profileItem => console.log(profileItem));
+const [name, github]  = profileDataArgs;
 
 
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-printProfileData(profileDataArgs);
+    console.log('Portfoliio Complete! Checkout index.html to see the output!');
+});
